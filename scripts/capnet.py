f9f934a68512d43256a51b4b8108dff3bbb061a8
jiachen <jiachen_lu1999@163.com>
2023-07-12 14:05:00 +0200

    update

--- scripts/capnet.py
+++ scripts/capnet.py
@@ -14,7 +14,6 @@ from model.softgroup import SoftGroup
 from model.relation_graph_module import GraphModule, bbox_pred_module_train, bbox_pred_module_val, bbox_pred_module_test
 from model.attention_module import AttentionModule
 from model.cac_caption_module import CACModule
-from model.lang_module import LangModule
 
 from model.evaluation import (PanopticEval, ScanNetEval, evaluate_offset_mae,
                               evaluate_semantic_acc, evaluate_semantic_miou)
@@ -114,13 +113,6 @@ class CapNet(pl.LightningModule):
                                             hidden_size=300, num_proposals=CONF.train_cfg.max_proposal_num,
                                             use_relation=self.use_relation)
 
-        # --------------------- Localization Module ------------------
-        # self.lang_module = LangModule()
-
-    # def forward(self, batch):
-    #     # no used
-    #     return None
-
     def training_step(self, batch):
         batch = self.softgroup_module.forward(batch, mode='train')
         semantic_loss = batch["detection_log_vars"]['semantic_loss']
@@ -183,17 +175,6 @@ class CapNet(pl.LightningModule):
             self.caption_module.train()
             gen_result, sample_logprobs = self.caption_module.forward_sample_mc(data_dict=batch, beam_size=5)
 
-            # print(greedy_seq)
-            # print(gen_result)
-
-            # batch['greedy_lang_feat'], batch['greedy_lang_len'] = transform_lang_emb(
-            #     greedy_seq)  # batch_size, emb_size
-            # batch['sample_lang_feat'], batch['sample_lang_len'] = transform_lang_emb(
-            #     gen_result)  # batch_size*sample_topn, emb_size
-
-            # localization module
-            # batch = self.lang_module(batch)
-
             reward = get_self_critical_reward(greedy_seq, batch['lang_ids'], gen_result, batch)
             reward = torch.from_numpy(reward).cuda()
             out['reward'] = reward[:, 0].mean()
@@ -202,7 +183,6 @@ class CapNet(pl.LightningModule):
             reward = reward.reshape(-1)
             mask = (gen_result.data > 0).to(sample_logprobs)
             mask = mask * batch['good_clu_masks'].repeat_interleave(2).unsqueeze(1)
-            # print(mask)
             mask = mask.reshape(-1)
             sc_loss = - sample_logprobs * reward * mask
 
@@ -225,9 +205,9 @@ class CapNet(pl.LightningModule):
 
     def on_train_epoch_end(self):
         params = self.state_dict()
-        # 将参数转移到CPU上
+        # to cpu
         params_cpu = {k: v.cpu() for k, v in params.items()}
-        # 保存参数到文件
+        # save model file
         self.filename = 'model0623_'
         current_time = datetime.datetime.now()
         time_string = current_time.strftime("%H:%M:%S")
@@ -265,41 +245,28 @@ class CapNet(pl.LightningModule):
         proposals_idx = batch['proposals_idx']
 
         num_points = batch['coords_float'].shape[0]  # num_point
-        num_instances = cls_scores.size(0)  # proposal的数量
-        cls_scores = cls_scores.softmax(1)  # softmax分类得分
-        max_cls_score, final_cls = cls_scores.max(1)  # M, 1 和 M, 1 每个proposal得到的分类最高分，以及属于哪个类
+        num_instances = cls_scores.size(0)  # #proposal
+        cls_scores = cls_scores.softmax(1)  # softmax score
+        max_cls_score, final_cls = cls_scores.max(1)  # M, 1 和 M, 1 best score of each proposal
 
         mask_pred = torch.zeros((num_instances, num_points), dtype=torch.int, device='cuda')  # M, num_point
-        # 旧的
-        # for i in range(num_instances):
-        #     cur_mask_scores = mask_scores[:, final_cls[i]]  # N, 1， N个点对第i个proposal的类的mask score
-        #     mask_inds = cur_mask_scores > -0.5  # threshold 取mask高于阈值的点
-        #     cur_proposals_idx = proposals_idx[mask_inds].long()
-        #     mask_pred[
-        #         cur_proposals_idx[:, 0], cur_proposals_idx[:, 1]] = 1  # M , num_point 表示有哪些点可能属于这个proposal对应的cls
-        #
-        # clu_point = torch.zeros((num_instances, num_points), dtype=torch.int, device='cuda')  # M, num_point
-        # for i in range(num_instances):
-        #     clu_point[
-        #         proposals_idx[:, 0].long(), proposals_idx[:, 1].long()] = 1  # M , num_point 表示有哪些点被group到这个proposal
 
-        # 新的
         for instance_idx in range(num_instances):
-            cur_mask_scores = mask_scores[:, final_cls[instance_idx]]  # N, 1， N个点对第i个proposal的类的mask score
-            mask_inds = cur_mask_scores > -0.5  # threshold 取mask高于阈值的点
+            cur_mask_scores = mask_scores[:, final_cls[instance_idx]]  # N, 1， mask score for point in class i
+            mask_inds = cur_mask_scores > -0.5  # threshold assign mask to points
             cur_proposals_idx = proposals_idx[mask_inds].long()
 
-            # M , num_point 表示有哪些点可能属于这个proposal对应的cls
+            # M , num_point
             mask_pred[instance_idx, cur_proposals_idx[:, 1]] = 1
 
         clu_point = torch.zeros((num_instances, num_points), dtype=torch.int, device='cuda')  # M, num_point
         # M , num_point 表示有哪些点被group到这个proposal
         clu_point[proposals_idx[:, 0].long(), proposals_idx[:, 1].long()] = 1
 
-        final_proposals = clu_point * mask_pred  # M ,num_point 最终结果，最终的proposal中有哪些点
+        final_proposals = clu_point * mask_pred  # M ,num_point final results points belong to the proposal
 
         final_proposals = final_proposals.cpu().numpy()
-        pred_bbox = np.zeros((num_instances, 6))  # M, 6 存储每个proposal的bbox的center和size
+        pred_bbox = np.zeros((num_instances, 6))  # M, 6 save each proposal bbox center and size
         for i in range(num_instances):
             idx = (final_proposals[i] == 1)
             object_points = batch['coords_float'][idx].cpu().numpy()
@@ -319,10 +286,6 @@ class CapNet(pl.LightningModule):
 
         _, batch['object_assignment'], _, _ = nn_distance(torch.from_numpy(pred_bbox[:, 0:3]).unsqueeze(0).cuda(),
                                                           batch['center_label'])  # 1, M
-
-        # assign predicted boxes to GTs
-        # assignments = box_assignment(batch['gt_box_corner_label'], pred_box_corner, batch['gt_box_masks'])
-        # batch['object_assignment'] = assignments['per_gt_prop_inds']
 
         # pick out object ids of detected objects
         detected_object_ids = torch.gather(batch["scene_object_ids"], 1, batch["object_assignment"])  # 1, M
@@ -368,7 +331,7 @@ class CapNet(pl.LightningModule):
                 except KeyError:
                     continue
 
-        self.n_preds += num_instances  # 记录pred_bbox的总数
+        self.n_preds += num_instances  # #pred_bbox
 
         for prop_id in range(num_instances):
             scene_id = str(batch['scan_ids'][0])
@@ -380,7 +343,7 @@ class CapNet(pl.LightningModule):
                 try:
                     ann_list = list(self.organized[scene_id][object_id].keys())
                     object_name = self.organized[scene_id][object_id][ann_list[0]]["object_name"]
-                    self.candidates[key] = [caption_decoded]  # 所有pred的句子
+                    self.candidates[key] = [caption_decoded]  # all pred descriptions
                     self.corpus[key] = self.corpus["{}|{}|{}".format(scene_id, object_id, object_name)]
 
                 except KeyError:
@@ -452,7 +415,7 @@ class CapNet(pl.LightningModule):
         current_time = datetime.datetime.now()
         time_string = current_time.strftime("%Y-%m-%d %H:%M:%S")
         with open("/home/luk/DenseCap/outputs/captioning_score.txt", "a") as file:
-            file.write('当前时间： ' + time_string + '\n')
+            file.write('Current Time： ' + time_string + '\n')
             file.write(self.filename + '\n')
             file.write('----Recall_scores----\n')
             file.write('CIDEr_recall is: {:.15f}\n'.format(cider_recall))
@@ -486,65 +449,28 @@ class CapNet(pl.LightningModule):
         print('METEOR_F1 is:', meteor_f1)
         print('ROUGE_F1 is:', rouge_f1)
 
-        # bleu_recall = capblue.Bleu(4).compute_score(self.corpus, self.candidates)
-        # cider_recall = capcider.Cider().compute_score(self.corpus, self.candidates)
-        # rouge_recall = caprouge.Rouge().compute_score(self.corpus, self.candidates)
-        # meteor_recall = capmeteor.Meteor().compute_score(self.corpus, self.candidates)
-        #
-        # self.logger.experiment.add_scalar("CIDEr", cider_recall[0], global_step=self.current_epoch)
-        # self.logger.experiment.add_scalar("BLEU-4", bleu_recall[0][3], global_step=self.current_epoch)
-        # self.logger.experiment.add_scalar("METEOR", rouge_recall[0], global_step=self.current_epoch)
-        # self.logger.experiment.add_scalar("ROUGE", rouge_recall[0], global_step=self.current_epoch)
-        #
-        # print('CIDEr_recall is:', cider_recall[0])
-        # print('BLEU-4_recall is:', bleu_recall[0][3])
-        # print('METEOR_recall is:', meteor_recall[0])
-        # print('ROUGE_recall is:', rouge_recall[0])
-        # print(cider_recall[1])
-        # print(meteor_recall[1])
-        # print(rouge_recall[1])
-        #
-        # bleu_precision = capblue.Bleu(4).compute_score(self.corpus_precision, self.candidates_precision)
-        # cider_precision = capcider.Cider().compute_score(self.corpus_precision, self.candidates_precision)
-        # rouge_precision = caprouge.Rouge().compute_score(self.corpus_precision, self.candidates_precision)
-        # meteor_precision = capmeteor.Meteor().compute_score(self.corpus_precision, self.candidates_precision)
-        #
-        # print('CIDEr_precision is:', cider_precision[0])
-        # print('BLEU-4_precision is:', bleu_precision[0][3])
-        # print('METEOR_precision is:', meteor_precision[0])
-        # print('ROUGE_precision is:', rouge_precision[0])
-        #
-        # cider_f1 = 2 * cider_recall[0] * cider_precision[0] / (cider_recall[0] + cider_precision[0])
-        # bleu_f1 = 2 * bleu_recall[0][3] * bleu_precision[0][3] / (bleu_recall[0][3] + bleu_precision[0][3])
-        # meteor_f1 = 2 * meteor_recall[0] * meteor_precision[0] / (meteor_recall[0] + meteor_precision[0])
-        # rouge_f1 = 2 * rouge_recall[0] * rouge_precision[0] / (rouge_recall[0] + rouge_precision[0])
-        #
-        # print('CIDEr_F1 is:', cider_f1)
-        # print('BLEU-4_F1 is:', bleu_f1)
-        # print('METEOR_F1 is:', meteor_f1)
-        # print('ROUGE_F1 is:', rouge_f1)
-
         return None
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        # optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
 
-        scheduler = StepLR(optimizer, step_size=1, gamma=0.9)  # 每个epoch后，学习率乘0.9
+        # scheduler = StepLR(optimizer, step_size=1, gamma=0.9)  # 每个epoch后，学习率乘0.9
+        # return [optimizer], [scheduler]
 
-        # opt_params = []
-        # opt_params.append({'params': self.softgroup_module.parameters(), 'weight_decay': 0})
-        #
-        # if self.use_relation:
-        #     opt_params.append({'params': self.relation_graph_module.parameters(), 'weight_decay': 1e-5})
-        #
-        # if (not self.use_cac) and self.use_attention:
-        #     opt_params.append({'params': self.attention_module.parameters(), 'weight_decay': 1e-5})
-        #
-        # opt_params.append({'params': self.caption_module.parameters(), 'weight_decay': 1e-5})
-        #
-        # optimizer = torch.optim.Adam(opt_params, lr=0.001)
+        opt_params = []
+        opt_params.append({'params': self.softgroup_module.parameters(), 'weight_decay': 0})
 
-        return [optimizer], [scheduler]
+        if self.use_relation:
+            opt_params.append({'params': self.relation_graph_module.parameters(), 'weight_decay': 1e-5})
+
+        if (not self.use_cac) and self.use_attention:
+            opt_params.append({'params': self.attention_module.parameters(), 'weight_decay': 1e-5})
+
+        opt_params.append({'params': self.caption_module.parameters(), 'weight_decay': 1e-5})
+
+        optimizer = torch.optim.Adam(opt_params, lr=0.001)
+
+        return optimizer
 
 
 def get_self_critical_reward(greedy_seq, data_gts, gen_result, data_dict):
@@ -555,7 +481,7 @@ def get_self_critical_reward(greedy_seq, data_gts, gen_result, data_dict):
     gen_result = gen_result.data.cpu().numpy()
     greedy_seq = greedy_seq.data.cpu().numpy()
 
-    # 将caption转化为字符串形式用于做reward
+    # caption to staring for reward
     def array_to_str(arr):
         out = ''
         if str(arr[0]) != '2':
@@ -571,9 +497,9 @@ def get_self_critical_reward(greedy_seq, data_gts, gen_result, data_dict):
     res = OrderedDict()
     gts = OrderedDict()
 
-    for i in range(gen_result_size):  # 前为采样结果
+    for i in range(gen_result_size):  # sample results
         res[i] = [array_to_str(gen_result[i])]
-    for i in range(batch_size):  # 后为greedy结果
+    for i in range(batch_size):  # greedy results
         res[gen_result_size + i] = [array_to_str(greedy_seq[i])]
 
     for i in range(batch_size):
@@ -585,8 +511,6 @@ def get_self_critical_reward(greedy_seq, data_gts, gen_result, data_dict):
     gts_.update({i + gen_result_size: gts[i] for i in range(batch_size)})
 
     _, cider_scores = capcider.Cider().compute_score(gts_, res__)
-    # print(cider_scores)
-    # print('Cider scores:', _)
 
     scores = cider_scores
 
@@ -613,24 +537,3 @@ def rl_crit(input, seq, reward, reduction='mean'):
         output = torch.sum(output) / torch.sum(mask)
 
     return output
-
-# def transform_lang_emb(sequence):
-#     num_result = sequence.shape[0]
-#     seq_len = sequence.shape[1]
-#
-#     lang_emb = torch.zeros(num_result, seq_len + 1,
-#                            self.caption_module.emb_size).cuda()  # B, max_len(30), 300
-#
-#     for i in range(num_result):
-#         lang_emb[i][0] = torch.FloatTensor(self.embeddings['sos']).cuda()
-#         for j in range(seq_len):
-#             idx = sequence[i][j]
-#             word = self.vocabulary["idx2word"][str(idx.item())]
-#             emb = torch.FloatTensor(self.embeddings[word]).cuda()  # emb_size
-#             lang_emb[i][j + 1] = emb
-#             if word == 'eos':
-#                 break
-#
-#     lang_len = torch.sum((lang_emb[:, :, 0] != 0), dim=-1)  # B
-#
-#     return lang_emb, lang_len
